d90ca26d9de7e44899a8af6070825a910c8129f4
freethlua <freethlua@gmail.com>
2017-06-28 06:56:45 +0530

    wp

--- build/app.js
+++ build/app.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/individualogist-whats-your-archetype/";
+/******/ 	__webpack_require__.p = "/wp-content/themes/individualogist/whats-your-archetype/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 361);
